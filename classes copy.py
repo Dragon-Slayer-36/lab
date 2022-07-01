437ba2da84df7def0f27797a022bd23bb858fed8
Dragon-Slayer-36 <108504430+Dragon-Slayer-36@users.noreply.github.com>
2022-07-01 17:25:07 -0500

    Update classes copy.py
    Docstrings and type hinting comments were added throughout the classes program.

--- classes copy.py
+++ classes copy.py
@@ -1,16 +1,29 @@
 class Television:
+    """
+    A class representing details for a television object via the using multiple functions below.   
+    """
+    
     MIN_CHANNEL = 0     # Minimum TV channel
     MAX_CHANNEL = 3     # Maximum TV channel
 
     MIN_VOLUME = 0      # Minimum TV volume
     MAX_VOLUME = 2      # Maximum TV volume
 
-    def __init__(self):
-        self.__channel = 0
-        self.__volume = 0
-        self.__status = 'False'
+    def __init__(self) -> None:
+        """
+        Constructor to create initial state of a television object.   
+        """
+        
+        self.__channel = 0          #Default TV channel
+        self.__volume = 0           #Default TV volume
+        self.__status = 'False'     #Default TV power
 
-    def power(self):
+    def power(self) -> str:
+        """
+        Method to determine and modify a television's power status.
+        :return: Television's power status.
+        """
+        
         if self.__status == 'False':
             self.__status = 'True'
             return self.__status
@@ -18,7 +31,12 @@ class Television:
             self.__status = 'False'
             return self.__status
 
-    def channel_up(self):
+    def channel_up(self) -> int:
+        """
+        Method to determine and/or modify a television's channel up by one with a maximum channel limit of 3.
+        :return: Television's channel number.
+        """
+        
         if self.__status == 'True':
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
@@ -28,7 +46,12 @@ class Television:
         else:
             return self.__channel
 
-    def channel_down(self):
+    def channel_down(self) -> int:
+        """
+        Method to determine and/or modify television's channel down by one with a minimum channel limit of 3.
+        :return: Television's channel number.        
+        """
+        
         if self.__status == 'True':
             if self.__channel == Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
@@ -38,7 +61,12 @@ class Television:
         else:
             return self.__channel
 
-    def volume_up(self):
+    def volume_up(self) -> int:
+        """
+        Method to determine and/or modify a television's volume up by one with a maximum channel limit of 2.
+        :return: Television's volume number.
+        """
+        
         if self.__status == 'True':
             if self.__volume == Television.MAX_VOLUME:
                 return self.__volume
@@ -48,7 +76,12 @@ class Television:
         else:
             return self.__volume
 
-    def volume_down(self):
+    def volume_down(self) -> int:
+        """
+        Method to determine and/or modify a television's volume up by one with a minimum channel limit of 0.
+        :return: Television's volume number.
+        """
+        
         if self.__status == 'True':
             if self.__volume == Television.MIN_VOLUME:
                 return self.__volume
@@ -59,5 +92,10 @@ class Television:
             return self.__volume
 
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Constructor to display details of a television object as string. 
+        :return: Print all details of a television object in a formatted string.
+        """
+        
         return f'TV status: Is on = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
